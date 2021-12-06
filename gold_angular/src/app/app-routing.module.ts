81b9f24cc8757986c9655970113f855c0aeb69a0
sukantahui <huisukanta77@gmail.com>
2021-12-06 12:37:18 +0530

    working resolver

--- gold_angular/src/app/app-routing.module.ts
+++ gold_angular/src/app/app-routing.module.ts
@@ -56,10 +56,9 @@ const routes: Routes = [
   {path: 'job', canActivate:  [AuthGuardService], component: JobComponent},
   {path: 'job/orderMaster/:orderNumber', canActivate:  [AuthGuardService], component: JobComponent},
 
-
-  {path: 'job_task', canActivate : [AuthGuardService], component: JobTaskComponent
-    , resolve: {data: JobTaskResolver}
-    },
+  {path: 'job_task', canActivate : [AuthGuardService], component: JobTaskComponent,
+    resolve: {jobTask: JobTaskResolver}
+  },
 
 
   {path: 'cash_payment', canActivate : [AuthGuardService], component: PaymentComponent},
