9ca38ef5f14b7bc6a3a31a7dd2df17958cf9c951
ritajaghosh <ritajaghosh@gmail.com>
2021-12-25 15:24:29 +0530

    new tag folder created

---
+++ new_tag/main.py
@@ -0,0 +1,31 @@
+import eel
+import requests
+from random import randint
+  
+eel.init("web")  
+  
+# Exposing the random_python function to javascript
+@eel.expose    
+def random_python():
+    print("Random function running")
+    return randint(1,100)
+
+# using the eel.expose command  
+@eel.expose  
+# defining the function for addition of two numbers  
+def add(data_1, data_2):  
+    int1 = int(data_1)  
+    int2 = int(data_2)  
+    output = int1 + int2  
+    return output  
+
+@eel.expose
+def fetchTagDetails(jojobNumberbId):
+    response = requests.get("http://127.0.0.1/gold_project/new_gold_api/public/api/dev/job/tag/jobMaster/JOB-00001-2122")
+    if response.status_code==200:
+        jobDetails = response.json().get('data')
+        print(jobDetails)
+    return jobDetails;
+  
+# Start the index.html file
+eel.start("index.html")
---
+++ new_tag/web/script.js
@@ -0,0 +1,26 @@
+
+  
+  $(document).ready(function() {
+
+    $('#generate-random').click(function(){
+        eel.random_python()(function(number){
+          $('.random_number').text(number);
+        });
+    });
+
+    $("#add-numbers").click(function() {
+      var data_1 = $("#int1").val();  
+      var data_2 = $("#int2").val();  
+      eel.add(data_1, data_2)(function(output){
+          $('#res').val(output);
+      })
+    });
+    
+    $("#fetch-request").click(function() { 
+      eel.fetchTagDetails('JOB-00001-2122')(function(requestData){
+        var jsonobj = requestData;
+        console.log(jsonobj);
+          $('#request-output').text(jsonobj.modelNumber);
+      })
+    });
+});
