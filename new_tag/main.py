822ce1914a9e999e0aad52ecd452cfe8fbed70a3
sukantahui <huisukanta77@gmail.com>
2021-12-28 14:01:31 +0530

    some problem

--- new_tag/main.py
+++ new_tag/main.py
@@ -1,9 +1,122 @@
 import eel
 import requests
 import json
+import pandas as pd
 
   
 eel.init("web")  
+
+def add_tags(tag, word):
+	return "<%s>%s</%s>" % (tag, word, tag)
+def add_td(word):
+    return "<td>%s</td>" % (word)
+
+
+
+
+
+def get_data():
+    dict_data = [
+        {
+        'duration': 0.7,
+        'project_id': 3,
+        'resource':  'Arya Stark',
+        'activity':  'Development'
+    },
+    {
+        'duration': 0.9,
+        'project_id': 4,
+        'resource':  'Ned Stark',
+        'activity':  'Development'
+    },
+    {
+        'duration': 2.88,
+        'project_id': 7,
+        'resource':  'Robb Stark',
+        'activity':  'Development'
+    },
+    {
+        'duration': 0.22,
+        'project_id': 9,
+        'resource':  'Jon Snow',
+        'activity':  'Support'
+    },
+    {
+        'duration': 0.3,
+        'project_id': 9,
+        'resource':  'Jon Snow',
+        'activity':  'Support'
+    },
+    {
+        'duration': 2.15,
+        'project_id': 3,
+        'resource':  'Arya Stark',
+        'activity':  'Practise'
+    },
+    {
+        'duration': 3.35,
+        'project_id': 4,
+        'resource':  'Sansa Stark',
+        'activity':  'Development'
+    },
+    {
+        'duration': 2.17,
+        'project_id': 9,
+        'resource':  'Rickon Stark',
+        'activity':  'Development'
+    },
+    {
+        'duration': 1.03,
+        'project_id': 4,
+        'resource':  'Benjan Stark',
+        'activity':  'Design'
+    },
+    {
+        'duration': 1.77,
+        'project_id': 4,
+        'resource':  'Bran Stark',
+        'activity':  'Testing'
+    },
+    {
+        'duration': 1.17,
+        'project_id': 4,
+        'resource':  'Ned Stark',
+        'activity':  'Development'
+    },
+    {
+        'duration': 0.17,
+        'project_id': 9,
+        'resource':  'Jon Snow',
+        'activity':  'Support'
+    },
+    {
+        'duration': 1.77,
+        'project_id': 3,
+        'resource':  'catelyn stark',
+        'activity':  'Development'
+    },
+    {
+        'duration': 0.3,
+        'project_id': 9,
+        'resource':  'Jon Snow',
+        'activity':  'Support'
+    },
+    {
+        'duration': 0.45,
+        'project_id': 9,
+        'resource':  'Jon Snow',
+        'activity':  'Support'
+    }
+    ]
+
+    df = pd.DataFrame(dict_data)
+    dfg = df.groupby(['project_id', 'resource', 'activity']).sum()
+    k = dfg.to_html('result.html')
+    return k
+
+
+
+
 
 @eel.expose
 def fetchTagDetails(jojobNumberbId):
@@ -24,9 +137,14 @@ def fillTable():
         x='<table class= "table"><thead> <tr><th>Job Number</th><th>Date</th><th>Quantity</th><th>Size</th></tr>  </thead>  <tbody>'
         for i in saveJobDetails:
             # print(i)
-            x += '<tr><td>'+str(i['job_number'])+'</td><td>'+str(i['date'])+'</td><td>'+str(i['quantity'])+'</td><td>'+str(i['size'])+'</td></tr>'
+            x += '<tr>'
+            x += add_tags('td',str(i['job_number']))
+            x += add_td(str(i['date']))
+            x += '<td>'+str(i['quantity'])+'</td>'
+            x += '<td>'+str(i['size'])+'</td>'
+            x += '</tr>'
         x+= '<tbody></table>'
-    return x
+    return get_data()
   
 # Start the index.html file
 eel.start("index.html")
