12807203a091e272e2d69ebb3163c51fbfeb446f
sukantahui <huisukanta77@gmail.com>
2021-12-31 14:05:07 +0530

    Working on bill

--- gold_angular/src/app/pages/bill/bill.component.ts
+++ gold_angular/src/app/pages/bill/bill.component.ts
@@ -4,6 +4,8 @@ import {FinishedJobs} from '../../models/finishedJobs';
 import {OrderDetail} from '../../models/orderDetail.model';
 import {JobService} from '../../services/job.service';
 import {FormControl} from '@angular/forms';
+import {environment} from '../../../environments/environment';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-bill',
@@ -14,15 +16,19 @@ export class BillComponent implements OnInit {
   public searchTerm: string;
   finshedJobs: FinishedJobs[] = [];
   orderDetails: OrderDetail[];
+  isProduction = environment.production;
 
   filter = new FormControl('');
-  page: number;
-  pageSize: number;
+  page = 1;
+  pageSize = 15;
   p = 1;
+  showDeveloperDiv: true;
+  billableCustomers: any;
 
-  constructor(private  billService: BillService , private  jobService: JobService) {
-    this.page = 1;
-    this.pageSize = 15;
+  constructor(private route: ActivatedRoute, private  billService: BillService , private  jobService: JobService) {
+    this.route.data.subscribe((response: any) => {
+      this.billableCustomers = response.bill.billableCustomers.data;
+    });
   }
 
   ngOnInit(): void {
--- new_gold_api/app/Http/Controllers/BillController.php
+++ new_gold_api/app/Http/Controllers/BillController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\JobDetail;
 use App\Models\JobMaster;
 use App\Models\OrderDetail;
+use App\Models\OrderMaster;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -12,11 +13,12 @@ class BillController extends ApiController
 {
     public function getBillableCustomers(){
         $OrderDetailIds = JobMaster::whereStatusId(100)->pluck('order_details_id');
-        $order_details = OrderDetail::select(DB::raw('people.user_name as customer_name'),DB::raw('count(order_masters.order_number) as no_of_orders'))
-                        ->join('order_masters','order_details.order_master_id','order_masters.id')
+        $orderMasterIds = OrderDetail::whereIn('id',$OrderDetailIds)->distinct()->pluck('order_master_id');
+
+        $order_details = OrderMaster::select('people.id',DB::raw('people.user_name as customer_name'),DB::raw('count(order_masters.order_number) as no_of_orders'))
                         ->join('people','people.id','order_masters.person_id')
                         ->groupBy('people.user_name')
-                        ->whereIn('order_details.id',$OrderDetailIds)->get();
+                        ->whereIn('order_masters.id',$orderMasterIds)->get();
         return $this->successResponse($order_details);
     }
 }
